aafc7f7f3c96c449239cda2889139028ec060add
陶智 <taozhi@tuliu.com>
2021-03-18 16:00:34 +0800

    update theme and add google analytics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,10 +2,28 @@ const path = require("path");
 
 module.exports = {
   title: "新时空前端 / 博客",
-  theme: "simple",
   dest: path.resolve(__dirname, "../../dist"),
   themeConfig: {
     author: "nst-fe",
   },
+  head: [
+    [
+      "script",
+      {
+        src: "https://www.googletagmanager.com/gtag/js?id=G-1PWBM2M60V",
+        async: true,
+      },
+    ],
+    [
+      "script",
+      {},
+      `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-1PWBM2M60V');
+      `,
+    ],
+  ],
   plugins: ["permalink-pinyin"],
 };
---
+++ docs/.vuepress/theme/components/FooterBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer class="footer">
+    © {{ thisYear }}
+    <i v-if="author" class="icon-user" />
+    <a href="https://www.nst-fe.site/" target="_blank">
+      {{ author }}
+    </a>
+    <br />
+    Powered by
+    <a href="https://vuepress.vuejs.org/" rel="noopener" target="_blank"
+      >VuePress</a
+    >
+    | Theme
+    <a
+      href="https://github.com/viko16/vuepress-theme-simple"
+      rel="noopener"
+      target="_blank"
+      >Simple</a
+    >
+  </footer>
+</template>
+
+<script>
+export default {
+  computed: {
+    thisYear() {
+      return new Date().getFullYear();
+    },
+    author() {
+      return this.$themeConfig.author || "";
+    },
+  },
+};
+</script>
