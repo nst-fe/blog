edacec66319d3e361e1253302b74d1b7285a3fee
陶智 <taozhi@tuliu.com>
2021-03-19 15:06:22 +0800

    replace comment to Gittalk

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -25,20 +25,5 @@ module.exports = {
       `,
     ],
   ],
-  plugins: [
-    "permalink-pinyin",
-    [
-      "@vssue/vuepress-plugin-vssue",
-      {
-        // set `platform` rather than `api`
-        platform: "github",
-
-        // all other options of Vssue are allowed
-        owner: "nst-fe",
-        repo: "blog",
-        clientId: "7a482526c12e6a8123f0",
-        clientSecret: "4faf2e5ce70c1ef2328da0f6bd8306160a7b53cf",
-      },
-    ],
-  ],
+  plugins: ["permalink-pinyin"],
 };
